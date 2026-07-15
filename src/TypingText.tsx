fdc7693305ec0d6eb7a18de4ff76030db4739fc3
K.Charan <kattacharan32@gmail.com>
2026-07-15 13:33:57 +0530

    Update TypingText.tsx

--- src/TypingText.tsx
+++ src/TypingText.tsx
@@ -1,20 +1,21 @@
 import { useState, useEffect } from "react";
 
-function TypingText({ text }) {
+interface TypingTextProps {
+  text: string;
+}
+
+function TypingText({ text }: TypingTextProps) {
   const [displayText, setDisplayText] = useState("");
 
   useEffect(() => {
     let i = 0;
-
     const interval = setInterval(() => {
       setDisplayText(text.slice(0, i + 1));
       i++;
-
       if (i === text.length) {
         clearInterval(interval);
       }
     }, 50);
-
     return () => clearInterval(interval);
   }, [text]);
 
