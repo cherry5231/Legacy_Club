a6af62cf6a5a6ac1e51a276248f138cff295e56f
K.Charan <kattacharan32@gmail.com>
2026-07-15 13:39:06 +0530

    Update Signup.tsx

--- src/Signup.tsx
+++ src/Signup.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./Signup.css";
+import "./signup.css";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
